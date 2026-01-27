54f8147037a750c826702fe24cf5735eb3b7991a
BEKLEY Essozolam <essozolam@172-10-27-54.lightspeed.rlghnc.sbcglobal.net>
2026-01-27 16:12:24 +0100

    ajout de section OurWork + ServiceCard

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import TrustedBy from './components/TrustedBy'
 import Services from './components/Services'
+import OurWork from './components/OurWork'
 
 
 const App = () => {
@@ -13,7 +14,7 @@ const App = () => {
       <Hero/>
       <TrustedBy/>
       <Services/>
-     
+      <OurWork/>
     </div>
   )
 }
--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,25 +1,41 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 
 
 const ServiceCard = ({ service, index }) => {
   const [position, setPosition] = useState({ x: 0, y: 0 })
-  cont [ visible, setVisible ] = useState(false);
+  const [ visible, setVisible ] = useState(false);
+  const divRef = useRef(null)
+
+const handleMouseMove = (e) => {
+  const bounds = divRef.current.getBoundingClientRect()
+  setPosition({
+    x: e.clientX - bounds.left,
+    y: e.clientY - bounds.top
+  })
+}
+
   return (
-    <div className="relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10">
+    <div className="relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border
+     border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10"
+     onMouseEnter={() => setVisible(true)} onMouseLeave={() => setVisible(false)} 
+     ref={divRef} onMouseMove={handleMouseMove}>
       
       
       <div
-        className="pointer-events-none absolute z-0 w-[300px] h-[300px] rounded-full blur-2xl bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 mix-blend-lighten opacity-70 transition-opacity duration-500"
-        style={{
-          top: position.y - 150,
-          left: position.x - 150,
-        }}
-      />
+  className={`pointer-events-none blur-2xl rounded-full bg-gradient-to-r 
+    from-blue-500 via-indigo-500 to-purple-500 
+    w-[300px] h-[300px] absolute z-0 transition-opacity duration-500 
+    mix-blend-lighten ${visible ? 'opacity-70' : 'opacity-0'}`}
+  style={{
+    top: position.y - 150,
+    left: position.x - 150,
+  }}
+/>
+
 
     
       <div className="relative z-10 flex items-center gap-10 p-8 rounded-[10px] bg-white dark:bg-gray-900 transition-all hover:p-7.5 hover:m-0.5">
-        git 
       
         <div className="rounded-full bg-gray-100 dark:bg-gray-700">
           <img
