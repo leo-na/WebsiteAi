8afda6f80d253494c46ccabc9ecbdbd4f7aea8db
BEKLEY Essozolam <essozolam@172-10-27-54.lightspeed.rlghnc.sbcglobal.net>
2026-01-27 15:00:32 +0100

    set position

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react'
 
 const ServiceCard = ({ service, index }) => {
   const [position, setPosition] = useState({ x: 0, y: 0 })
-
+  cont [ visible, setVisible ] = useState(false);
   return (
     <div className="relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10">
       
@@ -19,7 +19,7 @@ const ServiceCard = ({ service, index }) => {
 
     
       <div className="relative z-10 flex items-center gap-10 p-8 rounded-[10px] bg-white dark:bg-gray-900 transition-all hover:p-7.5 hover:m-0.5">
-        
+        git 
       
         <div className="rounded-full bg-gray-100 dark:bg-gray-700">
           <img
