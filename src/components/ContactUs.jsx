02bd32a61d95225f164591a155efba21eaa6b02d
BEKLEY Essozolam <essozolam@172-10-27-54.lightspeed.rlghnc.sbcglobal.net>
2026-01-28 02:34:14 +0100

    nouvelles fonctions

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 import Title from './Title'
 import assets from '../assets/assets' 
+import { toast } from 'react-hot-toast'
 
 const ContactUs = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "e9950e81-fe10-4aed-bd31-fef0e21dbb10");
+
+    try{
+       const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      toast.success("Message sent successfully!");
+      event.target.reset();
+    } else {
+      toast.error(data.message)
+    }
+    } catch(error){
+      toast.error(data.message)
+    }
+
+    
+  };
   return (
     <div
       id="contact-us"
@@ -13,12 +41,13 @@ const ContactUs = () => {
         desc="From strategy to execution, we craft digital solutions that move your business forward."
       />
 
-      <form className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
+      <form onSubmit={onSubmit}   className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
   <div>
   <p className='mb-2 text-sm font-medium'>Your name</p>
   <div className='flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600'>
     <img src={assets.person_icon} alt="" />
     <input
+      name='name'
       type="text"
       placeholder="Enter your name"
       className='w-full p-3 text-sm outline-none' required
@@ -30,6 +59,7 @@ const ContactUs = () => {
   <div className='flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600'>
     <img src={assets.email_icon} alt="" />
     <input
+      name='email'
       type="email"
       placeholder="Enter your email"
       className='w-full p-3 text-sm outline-none' required
@@ -40,9 +70,10 @@ const ContactUs = () => {
 <div className='sm:col-span-2'>
   <p className='mb-2 text-sm font-medium'>Message</p>
   <textarea
+    name='message'
     rows={8}
     placeholder='Enter your message'
-    className='w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600'
+    className='w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600' required
   />
 </div>
 <button
